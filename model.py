a6cd5d6cedc2de4c7f2e3a54b15143832742d262
marcin <marcin.kapiszewski@gmail.com>
2024-06-04 02:00:49 +0200

    Optymalize model fit and predict

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
+import time
 import pandas as pd
 import numpy as np
 import os.path
 from sklearn.cluster import KMeans
 from sklearn.metrics import mean_squared_error
+from utils import *
+from tqdm import tqdm
+from collections import defaultdict
+from utils import *
 
 import pandas as pd
 import os
@@ -35,7 +40,7 @@ class Model:
     - getMSE(test): Calculates the mean squared error for the model's predictions.
     """
 
-    def __init__(self, ratings: pd.DataFrame, k=30, preprocessed_users_file_name="./data/usersTest.csv", users=None):
+    def __init__(self, ratings: pd.DataFrame, k=30, preprocessed_users_file_name="./data/usersTest.csv", users=None, log = False):
         """
         Initializes a Movie Recommendation System model.
 
@@ -48,6 +53,9 @@ class Model:
         self.ratings = ratings
         self.preprocessed_users_file_name = preprocessed_users_file_name
         self.k = k
+        self.fit_start_time = None
+        self.clusters = None
+        self.log = log
 
     def fit(self, movies: pd.DataFrame, update_users=False):
         """
@@ -57,15 +65,29 @@ class Model:
         - movies (pd.DataFrame): DataFrame containing movie data. Must have 'movieId' and 'genres' columns. Genres should be in format 'genre1|genre2|...'.
         - update_users (bool): Whether to update the user data file. Default is False.
         """
+        self.fit_start_time = time.time()
+        if self.log:
+            print("Fitting model...")
         movies.genres = movies.genres.str.split('|')
         self.movies = movies
         if not os.path.isfile(self.preprocessed_users_file_name) or update_users:
+            if self.log:
+                print("Selecting relevant genres...")
             selected_genres = self.get_relevant_genres()
+            if self.log:
+                print(f"Selected genres: {selected_genres} selected at {(time.time() - self.fit_start_time)/60} minute.")
+                print("Preprocessing users...")
             self.users = self.get_users(selected_genres)
+            if self.log:
+                print(f"Preprocessed users at {(time.time() - self.fit_start_time)/60} minute.")
         else:
             self.users = pd.read_csv(self.preprocessed_users_file_name)
+            if self.log:
+                print(f"Loaded preprocessed users at {(time.time() - self.fit_start_time)/60} minute.")
         self.save_users(self.preprocessed_users_file_name)
         self.clusterUsers()
+        if self.log:
+            print(f"Model fitted at {(time.time() - self.fit_start_time)/60} minute.")
 
     def save_users(self, path):
         """
@@ -76,7 +98,7 @@ class Model:
         """
         self.users.to_csv(path, index=False)
     
-    def get_relevant_genres(self, threshold=0.0):
+    def get_relevant_genres(self, threshold=0.01):
         """
         Returns a list of relevant movie genres based on the data.
 
@@ -102,12 +124,68 @@ class Model:
         Returns:
         - users (pd.DataFrame): DataFrame of users and their genre-based ratings.
         """
+        # Unique user IDs
         users = pd.DataFrame(self.ratings['userId'].unique(), columns=['userId'])
+        
+        # Merge movies and ratings on movieId once
         df = pd.merge(self.movies, self.ratings, on='movieId')
-        for genre in selected_genres:
+
+        # Dictionary to hold user ratings for each genre
+        genre_ratings_dict = {genre: pd.Series(dtype=float) for genre in selected_genres}
+        
+        for genre_index, genre in enumerate(selected_genres):
+            if self.log:
+                printProgressBar(genre_index, len(selected_genres), prefix="Preprocessing users", suffix="Complete", length=50)
+            
+            # Filter the DataFrame for the current genre
             genre_ratings = df[df['genres'].apply(lambda x: genre in x)]
-            users[f"{genre}"] = users.apply(lambda x: genre_ratings[genre_ratings['userId'] == x['userId']]['rating'].mean(), axis=1)
+            
+            # Group by userId and calculate mean rating for the current genre
+            genre_mean_ratings = genre_ratings.groupby('userId')['rating'].mean()
+            
+            # Add the results to the dictionary
+            genre_ratings_dict[genre] = genre_mean_ratings
+        
+        # Convert the dictionary to a DataFrame
+        genre_ratings_df = pd.DataFrame(genre_ratings_dict)
+        
+        # Merge the users DataFrame with the genre ratings DataFrame
+        users = users.merge(genre_ratings_df, left_on='userId', right_index=True, how='left')
+        
+        if self.log:
+            printProgressBar(len(selected_genres), len(selected_genres), prefix="Preprocessing users", suffix="Complete", length=50)
+        
         return users
+    
+    def get_users_2(self, selected_genres):
+        bar_step = len(self.ratings)/10000
+        users_genres_dict = defaultdict(lambda: defaultdict(lambda: [0, 0]))
+        users = pd.DataFrame(columns=["userId"] + selected_genres)
+        ratings = pd.merge(self.ratings, self.movies[["movieId", "genres"]], on='movieId')
+        for index, row in ratings.iterrows():
+            if index % bar_step == 0:
+                if self.log:
+                    printProgressBar(index/bar_step, len(ratings)/bar_step, prefix="Iterating through ratings:", suffix="Complete", length=50)
+            for genre in row["genres"]:
+                users_genres_dict[row["userId"]][genre][1] += row["rating"]
+                users_genres_dict[row["userId"]][genre][0] += 1
+        if self.log:
+            printProgressBar(len(ratings)/bar_step, len(ratings)/bar_step, prefix="Iterating through ratings:", suffix="Complete", length=50)
+        users["userId"] = users_genres_dict.keys()
+        for idx, genre in enumerate(selected_genres):
+            if self.log:
+                printProgressBar(idx, len(selected_genres), prefix="Calculating genres mean:", suffix="Complete", length=50)
+            for user in users_genres_dict.keys():
+                num = users_genres_dict[user][genre][0]
+                sum = users_genres_dict[user][genre][1]
+                if num == 0:
+                    users.loc[user, genre] = np.nan
+                else:
+                    users.loc[user, genre] = sum/num
+        if self.log:
+            printProgressBar(len(selected_genres), len(selected_genres), prefix="Calculating genres mean:", suffix="Complete", length=50)
+        return users
+
     
     def clusterUsers(self):
         """
@@ -117,6 +195,9 @@ class Model:
         self.users = self.users.fillna(self.users.mean())
         usersCluster = kmeans.fit(self.users.drop('userId', axis=1))
         self.users['cluster'] = usersCluster.labels_
+
+        self.clusters = pd.merge(self.ratings[["userId","movieId","rating"]], self.users[['userId', 'cluster']], on='userId')
+        self.clusters = self.clusters.groupby(['cluster', 'movieId']).mean()
 
     def predict(self, userID, movieID):
         """
@@ -182,9 +263,10 @@ class Model:
         if userID not in self.users['userId'].values:
             return np.nan
         userCluster = self.users.loc[self.users['userId'] == userID, 'cluster'].values[0]
-        usersInCluster = self.users.loc[self.users['cluster'] == userCluster, 'userId']
-        ratings = self.ratings[(self.ratings['movieId'] == movieID) & (self.ratings['userId'].isin(usersInCluster))]
-        return ratings['rating'].mean()
+        if (userCluster, movieID) in self.clusters.index:
+            return self.clusters.loc[(userCluster, movieID), 'rating']
+        else:
+            return np.nan
     
     def getBaseline(self, test: pd.DataFrame):
         """
@@ -209,5 +291,6 @@ class Model:
         Returns:
         - mse (float): The mean squared error.
         """
-        test["prediction"] = test.apply(lambda x: self.predict(x["userId"], x["movieId"]), axis=1)
-        return mean_squared_error(y_true = test.rating, y_pred = test.prediction)
+        tqdm.pandas()
+        test["prediction"] = test.progress_apply(lambda x: self.predict(x["userId"], x["movieId"]), axis=1)
+        return mean_squared_error(y_true = test.rating/5, y_pred = test.prediction/5)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+# Print iterations progress
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+class Genre_ratings:
+    def __init__(self):
+        self.num = 0
+        self.sum = 0
+
+    def get_mean(self):
+        if self.num == 0:
+            return np.nan
+        return self.sum/self.num
